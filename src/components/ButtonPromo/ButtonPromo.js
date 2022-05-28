6eca5f90d03e9e591238e02595e0634c97433caa
IVKrylova <ivk.lis@yandex.ru>
2022-05-28 18:27:05 +0300

    feat: css and html code for promo in landing

---
+++ src/components/ButtonPromo/ButtonPromo.js
@@ -0,0 +1,10 @@
+import Button from '../Button/Button';
+import './ButtonPromo.css';
+
+function ButtonPromo() {
+  return (
+    <Button buttonText={'Узнать больше'} buttonClassName={'promo__button'} />
+  );
+}
+
+export default ButtonPromo;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,6 +1,15 @@
+import './Promo.css';
+import promoLogo from '../../images/promo-logo.svg';
+import ButtonPromo from '../ButtonPromo/ButtonPromo';
+
 function Promo() {
   return (
-    <section></section>
+    <section className="promo">
+      <img src={promoLogo} alt="Логотип в промо" className="promo__logo" />
+      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      <ButtonPromo />
+    </section>
   );
 }
 
