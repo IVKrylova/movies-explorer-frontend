71921d1cf0f72f311761aa8ad66df51a8fd07b16
IVKrylova <ivk.lis@yandex.ru>
2022-05-28 14:32:42 +0300

    feat: css and html code for header in app

---
+++ src/components/ButtonToAccount/ButtonToAccount.js
@@ -0,0 +1,10 @@
+import Button from '../Button/Button';
+import './ButtonToAccount.css';
+
+function ButtonToAccount() {
+  return (
+    <Button buttonText={'Аккаунт'} buttonClassName={'button__account'} />
+  );
+}
+
+export default ButtonToAccount;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
 import './Header.css';
 import logo from '../../images/logo.svg';
+import iconMenu from '../../images/header-icon-menu.svg';
 import { Link } from 'react-router-dom';
 import ButtonInHeader from '../ButtonInHeader/ButtonInHeader';
+import ButtonToAccount from '../ButtonToAccount/ButtonToAccount';
+import { useLocation } from 'react-router';
 
 function Header() {
+  // получаем текущий URL
+  const location = useLocation();
+  const currentUrl = location.pathname;
+
   return (
-    <header className="header header_place_landing">
+    <header className={`header ${currentUrl === '/' ? 'header_place_landing' : 'header_place_app'}`}>
       <img className="header__logo" src={logo} alt="Логотип" />
-      <Link to="/signup" className="header__link">Регистрация</Link>
-      <ButtonInHeader />
+      <div className={`header__landing-menu ${currentUrl === '/' ? '' : 'header__landing-menu_hidden'}`}>
+        <Link to="/signup" className="header__link">Регистрация</Link>
+        <ButtonInHeader />
+      </div>
+      <div className={`header__app-menu ${currentUrl === '/' ? 'header__app-menu_hidden' : ''}`}>
+        <img className="header__icon-menu header__icon-menu_hidden" src={iconMenu} alt="Иконка меню" />
+        <ButtonToAccount />
+      </div>
     </header>
   );
 }
