115fbc47213048cdc272a65c7cb956f28b1f90cc
IVKrylova <ivk.lis@yandex.ru>
2022-05-29 09:56:03 +0300

    style: change indents, add modifier for section-title

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -2,7 +2,7 @@ import './SectionTitle.css';
 
 function SectionTitle(props) {
   return (
-    <h2 className="section-title">{props.titleText}</h2>
+    <h2 className={`section-title ${props.classModifier}`}>{props.titleText}</h2>
   );
 }
 
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,6 +1,26 @@
+import SectionTitle from '../SectionTitle/SectionTitle';
+import './Techs.css';
+
 function Techs() {
   return (
-    <section></section>
+    <section className="techs">
+      <SectionTitle titleText='Технологии'
+        classModifier='section-title_line_black'/>
+      <h3 className="techs__subtitle">7 технологий</h3>
+      <p className="techs__text">
+        На курсе веб-разработки мы освоили технологии,
+        которые применили в дипломном проекте.
+      </p>
+      <ul className="techs__list">
+        <li className="techs__item">HTML</li>
+        <li className="techs__item">CSS</li>
+        <li className="techs__item">JS</li>
+        <li className="techs__item">React</li>
+        <li className="techs__item">Git</li>
+        <li className="techs__item">Express.js</li>
+        <li className="techs__item">mongoDB</li>
+      </ul>
+    </section>
   );
 }
 
