8abef0425a96d38daeafa1db909f9c08f3107aea
IVKrylova <ivk.lis@yandex.ru>
2022-05-29 21:50:43 +0300

    feat: html code portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,31 @@
+import './Portfolio.css'
+import linkIcon from '../../images/portfolio-link-icon.svg'
+
 function Portfolio() {
   return (
-    <section></section>
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a className="portfolio__link" target="_blank" href="https://ivkrylova.github.io/how-to-learn/index.html" rel="noreferrer">
+            Статичный сайт
+          </a>
+          <img src={linkIcon} alt="иконка-стрелка для ссылки на другой сайт" className="portfolio__icon" />
+        </li>
+        <li>
+          <a className="portfolio__link" target="_blank" href="https://ivkrylova.github.io/russian-travel/" rel="noreferrer">
+            Адаптивный сайт
+          </a>
+          <img src={linkIcon} alt="иконка-стрелка для ссылки на другой сайт" className="portfolio__icon" />
+        </li>
+        <li>
+          <a className="portfolio__link" target="_blank" href="https://mesto.ivkrylova.nomoredomains.work/" rel="noreferrer">
+            Одностраничное приложение
+          </a>
+          <img src={linkIcon} alt="иконка-стрелка для ссылки на другой сайт" className="portfolio__icon" />
+        </li>
+      </ul>
+    </section>
   );
 }
 
