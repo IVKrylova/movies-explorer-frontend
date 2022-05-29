c6e36ab06d31e411db2c55ae90f9d20c91918aa9
IVKrylova <ivk.lis@yandex.ru>
2022-05-29 13:31:55 +0300

    feat: css and html code for Techs

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -2,6 +2,7 @@ import './SectionTitle.css';
 
 function SectionTitle(props) {
   return (
+    /* ToDo белая линия при зазрешении > 768px в techs */
     <h2 className={`section-title ${props.classModifier}`}>{props.titleText}</h2>
   );
 }
