576f68078d3ef9ff71afe0e6716825f6feec9bab
IVKrylova <ivk.lis@yandex.ru>
2022-05-30 22:01:15 +0300

    fix: PixelPerfect

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,7 +6,7 @@ function Promo() {
   return (
     <section className="promo">
       <img src={promoLogo} alt="Логотип в промо" className="promo__logo" />
-      <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+      <h1 className="promo__title">Учебный проект студента факультета <span className="promo__title-span">Веб-разработки.</span></h1>
       <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <NavTabButton />
     </section>
