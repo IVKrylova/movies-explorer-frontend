15c123a918fa7f9258973c27c2bc5dc5096e1857
IVKrylova <ivk.lis@yandex.ru>
2022-06-02 10:34:49 +0300

    feat: add page 404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,19 @@
-import '/PageNotFound.css';
+import './PageNotFound.css';
+import { useHistory } from 'react-router-dom';
 
 const PageNotFound = _ => {
+  const history = useHistory();
+
   return (
-    <section></section>
+    <section className="page-not-found">
+      <p className="page-not-found__code">404</p>
+      <p className="page-not-found__text">Страница не найдена</p>
+      <button type="button"
+        className="page-not-found__button-go-back"
+        onClick={_ => history.goBack()}>
+        Назад
+      </button>
+    </section>
   );
 }
 
