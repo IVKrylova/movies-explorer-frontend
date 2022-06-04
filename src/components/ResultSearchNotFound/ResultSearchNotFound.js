2a78184579b534925eb702da9597e28f9d3a50a0
IVKrylova <ivk.lis@yandex.ru>
2022-06-04 09:56:48 +0300

    feat: add component ResultSearchNotFound

---
+++ src/components/ResultSearchNotFound/ResultSearchNotFound.js
@@ -0,0 +1,11 @@
+import './ResultSearchNotFound.css';
+
+const ResultSearchNotFound = _ => {
+  return (
+    <p className="result-not-found">
+      К сожалению, ничего не найдено
+    </p>
+  );
+}
+
+export default ResultSearchNotFound;
