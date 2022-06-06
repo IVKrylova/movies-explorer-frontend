27a9d9d59aaab35a36a3c8734eeffa7ac8e22cb5
IVKrylova <ivk.lis@yandex.ru>
2022-06-06 19:18:37 +0300

    fix: change movie hover handlers on page movies

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -12,14 +12,19 @@ const MoviesCard = props => {
 
   return (
     <li className={`movie ${props.currentUrl === '/saved-movies' ? 'movie_place_saved-movies' : ''}`}
-      onMouseOver={handleMouseOver} onMouseOut={props.onMouseOut}>
+      onMouseOver={props.currentUrl === '/saved-movies' ? handleMouseOver : undefined}
+      onMouseOut={props.currentUrl === '/saved-movies' ? props.onMouseOut : undefined}>
       <img src={props.image} alt={props.nameRU} className="movie__img" id={props.id} />
       <h3 className="movie__name">{props.nameRU}</h3>
       <p className="movie__duration">{props.duration}</p>
-      <button type="button" className={`movie__like ${props.currentUrl === '/movies' ? '' : 'movie__like_hidden'}`} aria-label="кнопка лайк"></button>
+      <button type="button"
+        className={`movie__like ${props.currentUrl === '/movies' ? '' : 'movie__like_hidden'}`}
+        aria-label="кнопка лайк">
+      </button>
       <button type="button"
         className={`movie__delete ${props.currentUrl === '/saved-movies' ? '' : 'movie__delete_hidden'} ${props.idCardHovered === props.id && screenWidth >= 768 ? 'movie__delete_visible' : ''}`}
-        aria-label="кнопка удалить из сохраненных"></button>
+        aria-label="кнопка удалить из сохраненных">
+      </button>
     </li>
   );
 }
