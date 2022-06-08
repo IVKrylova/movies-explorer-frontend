74551ca6649537a4b85967d1a55631d2ec5fbce5
IVKrylova <ivk.lis@yandex.ru>
2022-06-08 21:40:23 +0300

    refactor: code style

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,4 +1,4 @@
-import './Preloader.css'
+import './Preloader.css';
 
 const Preloader = props => {
   return (
@@ -10,4 +10,4 @@ const Preloader = props => {
   );
 }
 
-export default Preloader
+export default Preloader;
